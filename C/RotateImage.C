9fff05f4d1a64aaf42186abb52276dca859501dc
Darren <darren.chen@harman.com>
2018-03-20 00:56:10 +0800

    Rotate Image

--- C/RotateImage.C
+++ C/RotateImage.C
@@ -11,3 +11,24 @@ char* rotateImage(char* input, int width, int height) {
     }
     return output;
 }
+
+
+/*
+You are given an n x n 2D matrix representing an image.
+
+Rotate the image by 90 degrees (clockwise).
+
+Note:
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+*/
+void rotate(int** matrix, int matrixRowSize, int *matrixColSizes) {
+    for (int i = 0; i< matrixRowSize / 2; i++) {
+        for (int j = i; j < matrixRowSize - 1 - i; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[matrixRowSize - j - 1][i];
+            matrix[matrixRowSize - j - 1][i] = matrix[matrixRowSize - i - 1][matrixRowSize - j - 1];
+            matrix[matrixRowSize - i - 1][matrixRowSize - j - 1] = matrix[j][matrixRowSize - i - 1];
+            matrix[j][matrixRowSize - i - 1] = temp;
+        }
+    }
+}
