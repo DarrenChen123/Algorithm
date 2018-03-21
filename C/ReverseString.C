5ed134776ec7304de2fc1b4dc42866bc8b03f404
Darren <darren.chen@harman.com>
2018-03-21 23:45:07 +0800

    Reverse String

---
+++ C/ReverseString.C
@@ -0,0 +1,18 @@
+/*
+Write a function that takes a string as input and returns the string reversed.
+
+Example:
+Given s = "hello", return "olleh".
+*/
+char* reverseString(char* s) {
+    int length = 0;
+    while (s[length] != '\0') {
+        length++;
+    }
+    char * ret = (char *) malloc (sizeof(char) * (length + 1));
+    for (int i = 0; i < length; i++) {
+        ret[length - 1 - i] = s[i];
+    }
+    ret[length] = '\0';
+    return ret;
+}
