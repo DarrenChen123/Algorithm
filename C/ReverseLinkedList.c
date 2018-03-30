22c26d0e03dc596376dd584159f65a6565f86a6e
Darren Chen <Darren.Chen@harman.com>
2018-03-31 02:34:28 +0800

    Reverse linked list.

--- C/ReverseLinkedList.c
+++ C/ReverseLinkedList.c
@@ -7,5 +7,40 @@ Hint:
 A linked list can be reversed either iteratively or recursively. Could you implement both?
 
 */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+//Iterative
+struct ListNode* reverseList(struct ListNode* head) {
+    if (head == NULL) {
+        return NULL;
+    }
+    struct ListNode* current = head;
+    struct ListNode* previous = NULL;
+    while (current != NULL) {
+        struct ListNode* temp = current->next;
+        current->next = previous;
+        previous = current;
+        current = temp;
+    }
+    head = previous;
+    return head;
+}
 
-
+//Recursive
+struct ListNode* reverseList(struct ListNode* head) {
+    if (head == NULL) {
+        return NULL;
+    }
+    if (head->next == NULL) {
+        return head;
+    }
+    struct ListNode* ret = reverseList(head->next);
+    head->next->next = head;
+    head->next = NULL;
+    return ret;
+}
