43597a589a377d88a90deaf3667da4680c048a6b
Darren Chen <Darren.Chen@harman.com>
2018-04-02 16:45:26 +0800

    Linked list cycle.

---
+++ C/LinkedListCycle.c
@@ -0,0 +1,27 @@
+/*
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+*/
+
+bool hasCycle(struct ListNode *head) {
+    if (head == NULL || head->next == NULL) {
+        return false;
+    }
+    struct ListNode *slow = head->next;
+    struct ListNode *fast = head->next->next;
+    while (slow != NULL && fast != NULL) {
+        if (slow == fast) {
+            return true;
+        } else {
+            slow = slow->next;
+            if (fast->next) {
+                fast = fast->next->next;
+            } else {
+                return NULL;
+            }
+        }
+    }
+    return NULL;
+}
