fa5c0e2f004e408b89a61767a7c11920721a9ed9
Darren Chen <Darren.Chen@harman.com>
2018-04-04 22:58:20 +0800

    Symmetric tree.

--- C/SymmetricTree.c
+++ C/SymmetricTree.c
@@ -47,3 +47,103 @@ bool isSymmetric(struct TreeNode* root) {
     }
     return isMirror(root, root);
 }
+
+// Iterative
+typedef struct node {
+    struct TreeNode *item;
+    struct node *next;
+} Node;
+
+typedef struct queue {
+    Node * front;
+    Node * rear;
+    int items;
+} Queue;
+
+void initializeQueue (Queue * pq) {
+    pq->front = NULL;
+    pq->rear = NULL;
+    pq->items = 0;
+}
+
+bool queueIsFull (const Queue * pq) {
+    return pq->items == 1024;
+}
+
+bool queueIsEmpty (const Queue * pq) {
+    return pq->items == 0;
+}
+
+int queueItemCount (const Queue * pq) {
+    return pq->items;
+}
+
+bool enQueue (struct TreeNode * item, Queue * pq) {
+    Node * pnew;
+
+    if (queueIsFull(pq)) {
+        return false;
+    }
+    pnew = (Node *) malloc (sizeof(Node));
+    if (pnew == NULL) {
+        exit(1);
+    }
+    pnew->item = item;
+    pnew->next = NULL;
+    if (queueIsEmpty(pq)) {
+        pq->front = pnew;
+    } else {
+        pq->rear->next = pnew;
+    }
+    pq->rear = pnew;
+    pq->items++;
+    return true;
+}
+
+bool deQueue(struct TreeNode ** pitem, Queue * pq) {
+    Node * pt;
+    if (queueIsEmpty(pq)) {
+        return false;
+    }
+    *pitem = pq->front->item;
+    pq->front = pq->front->next;
+    free(pt);
+    pq->items--;
+    if (pq->items == 0) {
+        pq->rear =  NULL;
+    }
+    return true;
+}
+
+bool isSymmetric(struct TreeNode* root) {
+    if (root == NULL) {
+        return true;
+    }
+    if (root->left == NULL && root->right == NULL) {
+        return true;
+    }
+    Queue * queue = (Queue *) malloc (sizeof(Queue));
+    initializeQueue(queue);
+    enQueue(root, queue);
+    enQueue(root, queue);
+    while (!queueIsEmpty(queue)) {
+        struct TreeNode * tree1 = (struct TreeNode *) malloc (sizeof(struct TreeNode));
+        struct TreeNode * tree2 = (struct TreeNode *) malloc (sizeof(struct TreeNode));
+        deQueue(&tree1, queue);
+        deQueue(&tree2, queue);
+        if (tree1 == NULL && tree2 == NULL) {
+            continue;
+        }
+        if (tree1 == NULL || tree2 == NULL) {
+            return false;
+        }
+        if (tree1->val != tree2->val) {
+            return false;
+        }
+        enQueue(tree1->left, queue);
+        enQueue(tree2->right, queue);
+        enQueue(tree1->right, queue);
+        enQueue(tree2->left, queue);
+    }
+    return true;
+}
