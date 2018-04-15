11ce7fae9f75506d0fa9ab94e482b225357f5f1d
Darren Chen <Darren.Chen@harman.com>
2018-04-15 23:26:02 +0800

    Power of three.

---
+++ C/PowerOfThree.c
@@ -0,0 +1,26 @@
+/*
+Given an integer, write a function to determine if it is a power of three.
+
+Follow up:
+Could you do it without using any loop / recursion?
+*/
+
+// Recursion
+bool isPowerOfThree(int n) {
+    if (n <= 0) {
+        return false;
+    }
+    if (n == 1) {
+        return true;
+    } else if (n % 3 != 0) {
+        return false;
+    } else {
+        return isPowerOfThree(n / 3);
+    }
+}
+
+
+// Non-recursion
+bool isPowerOfThree(int n) {
+    return n > 0 && ((int)pow(3, (int)(log (INT_MAX) / log(3)))) % n == 0;
+}
