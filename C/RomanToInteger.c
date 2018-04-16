d6a8e0c5f3f2b8a5be3252ea288b2fd4ba019f62
Darren Chen <Darren.Chen@harman.com>
2018-04-17 01:04:53 +0800

    Roman to integer.

---
+++ C/RomanToInteger.c
@@ -0,0 +1,48 @@
+/*
+Given a roman numeral, convert it to an integer.
+
+Input is guaranteed to be within the range from 1 to 3999.
+*/
+
+int romanValue(char letter) {
+    switch (letter) {
+        case 'I':
+            return 1;
+        case 'V':
+            return 5;
+        case 'X':
+            return 10;
+        case 'L':
+            return 50;
+        case 'C':
+            return 100;
+        case 'D':
+            return 500;
+        case 'M':
+            return 1000;
+        default:
+            return 0;
+    }
+}
+bool leftIsSmaller(char left, char right) {
+    return romanValue(left) < romanValue(right);
+}
+
+int romanToInt(char* s) {
+    if (s == NULL) {
+        return 0;
+    }
+    int length = strlen(s);
+    if (length == 0) {
+        return 0;
+    }
+    int ret = 0;
+    for (int i = 0; i < length; i++) {
+        if (i == length - 1 || !leftIsSmaller(s[i], s[i + 1])) {
+            ret += romanValue(s[i]);
+        } else {
+            ret -= romanValue(s[i]);
+        }
+    }
+    return ret;
+}
