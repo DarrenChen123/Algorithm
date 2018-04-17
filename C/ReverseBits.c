41fb3866d23ffe568a5156e10ae26b5aac4b7b44
Darren Chen <Darren.Chen@harman.com>
2018-04-17 13:30:40 +0800

    Reverse bits.

---
+++ C/ReverseBits.c
@@ -0,0 +1,19 @@
+/*
+Reverse bits of a given 32 bits unsigned integer.
+
+For example, given input 43261596 (represented in binary as 00000010100101000001111010011100), return 964176192 (represented in binary as 00111001011110000010100101000000).
+
+Follow up:
+If this function is called many times, how would you optimize it?
+
+Related problem: Reverse Integer
+*/
+uint32_t reverseBits(uint32_t n) {
+    uint32_t ret = 0;
+    for (int i = 0; i < 32; i++) {
+        ret <<= 1;
+        ret |= n & 1;
+        n >>= 1;
+    }
+    return ret;
+}
