41507843783bc14bad30b5b76fc41cf0679d55f3
chenjia <chenjia2432@gmail.com>
2018-04-20 11:33:29 +0800

    Missing number.

---
+++ C/MissingNumber.c
@@ -0,0 +1,30 @@
+/*
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+Example 1
+
+Input: [3,0,1]
+Output: 2
+Example 2
+
+Input: [9,6,4,2,3,5,7,0,1]
+Output: 8
+
+Note:
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+*/
+
+int missingNumber(int* nums, int numsSize) {
+	char* key = (char *) malloc (sizeof(char) * numsSize);
+	memset(key, 0, sizeof(char) * numsSize);
+	for (int i = 0; i < numsSize; i++) {
+		key[nums[i] / 4] |= 1 << (nums[i] % 4);
+	}
+	for (int i = 0; i < numsSize; i++) {
+		if ((key[i / 4] >> (i % 4) & 1) == 0) {
+			return i;
+		}
+	}
+	free(key);
+	return numsSize;
+}
