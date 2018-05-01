f6946ccc40625bb39e3a4d69bf49b3837d04ea0e
chenjia <chenjia2432@gmail.com>
2018-05-01 18:46:36 +0800

    Binary tree inorder traversal.

--- C/BinaryTreeInorderTraversal.c
+++ C/BinaryTreeInorderTraversal.c
@@ -43,3 +43,63 @@ int* inorderTraversal(struct TreeNode* root, int* returnSize) {
     inorder(root, returnSize, ret);
     return ret;
 }
+
+// Iterative
+typedef struct stack {
+    struct TreeNode **items;
+    int maxSize;
+    int top;
+} Stack;
+
+void initializeStack (Stack * ps, int maxSize) {
+    struct TreeNode **items = (struct TreeNode **) malloc (sizeof(struct TreeNode*) * maxSize);
+    ps->top = 0;
+    ps->maxSize = maxSize;
+    ps->items = items;
+}
+
+int fullStack (const Stack * ps) {
+    return ps->top == ps->maxSize;
+}
+
+int emptyStack (const Stack * ps) {
+    return ps->top == 0;
+}
+
+int push (struct TreeNode * item, Stack * ps) {
+    if (ps->top == ps->maxSize) {
+        return 0;
+    } else {
+        ps->items[ps->top++] = item;
+        return 1;
+    }
+}
+
+struct TreeNode * pop(Stack * ps) {
+    if (ps->top == 0) {
+        return NULL;
+    } else {
+        ps->top--;
+        return ps->items[ps->top];
+    }
+}
+
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    int* ret = (int*) malloc (sizeof(int) * 1024);
+    *returnSize = 0;
+    Stack* stack = (Stack *) malloc (sizeof(Stack));
+    initializeStack(stack, 1024);
+    struct TreeNode* node = root;
+    while (node != NULL || !emptyStack(stack)) {
+        while (node != NULL) {
+            push (node, stack);
+            node = node->left;
+        }
+        if (!emptyStack(stack)) {
+            node = pop(stack);
+            ret[(*returnSize)++] = node->val;
+            node = node->right;
+        }
+    }
+    return ret;
+}
