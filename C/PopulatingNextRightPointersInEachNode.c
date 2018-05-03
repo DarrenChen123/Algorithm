057e73bd83c503600ddfb1fba6b55ac3cf44a40c
chenjia <chenjia2432@gmail.com>
2018-05-03 12:23:11 +0800

    Populating next right pointers in each node.

---
+++ C/PopulatingNextRightPointersInEachNode.c
@@ -0,0 +1,56 @@
+/*
+Given a binary tree
+
+struct TreeLinkNode {
+  TreeLinkNode *left;
+  TreeLinkNode *right;
+  TreeLinkNode *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+Note:
+
+You may only use constant extra space.
+Recursive approach is fine, implicit stack space does not count as extra space for this problem.
+You may assume that it is a perfect binary tree (ie, all leaves are at the same level, and every parent has two children).
+Example:
+
+Given the following perfect binary tree,
+
+     1
+   /  \
+  2    3
+ / \  / \
+4  5  6  7
+After calling your function, the tree should look like:
+
+     1 -> NULL
+   /  \
+  2 -> 3 -> NULL
+ / \  / \
+4->5->6->7 -> NULL
+*/
+
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  struct TreeLinkNode *left, *right, *next;
+ * };
+ *
+ */
+void connect(struct TreeLinkNode *root) {
+    if (!root) {
+        return NULL;
+    }
+    if (root->left) {
+        root->left->next = root->right;
+    }
+    if (root->right) {
+        root->right->next = root->next ? root->next->left : NULL;
+    }
+    connect(root->left);
+    connect(root->right);
+}
