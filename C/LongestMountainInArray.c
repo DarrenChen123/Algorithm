e662d254d5b2f0215020c2cad9962458561b541a
chenjia <chenjia2432@gmail.com>
2018-06-04 10:35:11 +0800

    Longest mountain in array.

---
+++ C/LongestMountainInArray.c
@@ -0,0 +1,56 @@
+/*
+Let's call any (contiguous) subarray B (of A) a mountain if the following properties hold:
+
+B.length >= 3
+There exists some 0 < i < B.length - 1 such that B[0] < B[1] < ... B[i-1] < B[i] > B[i+1] > ... > B[B.length - 1]
+(Note that B could be any subarray of A, including the entire array A.)
+
+Given an array A of integers, return the length of the longest mountain.
+
+Return 0 if there is no mountain.
+
+
+
+Example 1:
+
+Input: [2,1,4,7,3,2,5]
+Output: 5
+Explanation: The largest mountain is [1,4,7,3,2] which has length 5.
+Example 2:
+
+Input: [2,2,2]
+Output: 0
+Explanation: There is no mountain.
+
+
+Note:
+
+0 <= A.length <= 10000
+0 <= A[i] <= 10000
+*/
+
+int longestMountain(int* A, int ASize) {
+    if (ASize < 3) {
+        return 0;
+    }
+    int ret = 0;
+    for (int i = 1; i < ASize - 1; i++) {
+        if (A[i] > A[i - 1] && A[i] > A[i + 1]) {
+            int begin = i - 1;
+            int end = i + 1;
+            while (begin > 0 && A[begin] > A[begin - 1]) {
+                begin--;
+            }
+            while (end < ASize - 1 && A[end] > A[end + 1]) {
+                end++;
+            }
+            ret = ret > end - begin + 1 ? ret : end - begin + 1;
+            if (end < ASize - 1) {
+                i = end;
+            } else {
+                return ret;
+            }
+        }
+    }
+    return ret;
+}
