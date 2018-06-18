f02da99b65e9108c258bac743324f81ab28cff92
chenjia <chenjia2432@gmail.com>
2018-06-18 18:44:09 +0800

    Inorder successor in BST.

---
+++ C/InorderSuccessorInBST.c
@@ -0,0 +1,48 @@
+/*
+Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
+
+Note: If the given node has no in-order successor in the tree, return null.
+
+Example 1:
+
+Input: root = [2,1,3], p = 1
+
+  2
+ / \
+1   3
+
+Output: 2
+Example 2:
+
+Input: root = [5,3,6,2,4,null,null,1], p = 6
+
+      5
+     / \
+    3   6
+   / \
+  2   4
+ /
+1
+
+Output: null
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* inorderSuccessor(struct TreeNode* root, struct TreeNode* p) {
+    if (!root) {
+        return NULL;
+    }
+    if (root->val <= p->val) {
+        return inorderSuccessor(root->right, p);
+    } else {
+        struct TreeNode *left = inorderSuccessor(root->left, p);
+        return left ? left : root;
+    }
+}
