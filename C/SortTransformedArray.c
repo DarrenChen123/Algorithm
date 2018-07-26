c280fcc9639bd89d6dc9f53379fb136f35ee6138
chenjia <chenjia2432@gmail.com>
2018-07-27 00:05:51 +0800

    Sort transformed array.

--- C/SortTransformedArray.c
+++ C/SortTransformedArray.c
@@ -16,3 +16,41 @@ nums = [-4, -2, 2, 4], a = -1, b = 3, c = 5
 
 Result: [-23, -5, 1, 7]
 */
+
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* sortTransformedArray(int* nums, int numsSize, int a, int b, int c, int* returnSize) {
+    int i = 0;
+    int j = numsSize - 1;
+    *returnSize = numsSize;
+    int* res = (int*) malloc (sizeof(int) * (*returnSize));
+    int index = a >= 0 ? numsSize - 1 : 0;
+    while (i <= j) {
+        int res1 = cal(nums[i], a, b, c);
+        int res2 = cal(nums[j], a, b, c);
+        if (a >= 0) {
+            if (res1 >= res2) {
+                res[index--] = res1;
+                i++;
+            } else {
+                res[index--] = res2;
+                j--;
+            }
+        } else {
+            if (res1 >= res2) {
+                res[index++] = res2;
+                j--;
+            } else {
+                res[index++] = res1;
+                i++;
+            }
+        }
+    }
+    return res;
+}
+
+int cal(int x, int a, int b, int c) {
+    return a * x * x + b * x + c;
+}
