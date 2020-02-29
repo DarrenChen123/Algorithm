97fdf486962b4e91a993d0a1cb9f0a3eba258524
chenjia <chenjia2432@gmail.com>
2020-03-01 05:03:00 +0800

    Image smoother.

---
+++ C/ImageSmoother.c
@@ -0,0 +1,77 @@
+// Google
+/*
+Given a 2D integer matrix M representing the gray scale of an image, you need to design a smoother to make the gray scale of each cell becomes the average gray scale (rounding down) of all the 8 surrounding cells and itself. If a cell has less than 8 surrounding cells, then use as many as you can.
+
+Example 1:
+Input:
+[[1,1,1],
+ [1,0,1],
+ [1,1,1]]
+Output:
+[[0, 0, 0],
+ [0, 0, 0],
+ [0, 0, 0]]
+Explanation:
+For the point (0,0), (0,2), (2,0), (2,2): floor(3/4) = floor(0.75) = 0
+For the point (0,1), (1,0), (1,2), (2,1): floor(5/6) = floor(0.83333333) = 0
+For the point (1,1): floor(8/9) = floor(0.88888889) = 0
+Note:
+The value in the given matrix is in the range of [0, 255].
+The length and width of the given matrix are in the range of [1, 150].
+*/
+
+int** imageSmoother(int** M, int MSize, int* MColSize, int* returnSize, int** returnColumnSizes){
+	*returnSize = MSize;
+	*returnColumnSizes = (int *)malloc(sizeof(int) * MSize);
+	int **ret = (int **)malloc(sizeof(int *) * MSize);
+	memcpy(*returnColumnSizes, MColSize, sizeof(int) * MSize);
+	if (MSize == 0) {
+		return NULL;
+	}
+	for (int i = 0;i < MSize;i++) {
+		ret[i] = (int *)malloc(sizeof(int) * MColSize[i]);
+		for (int j = 0;j < MColSize[i];j++) {
+			/*
+			int sum = 0;
+			int count = 0;
+			if (i >= 1) {
+				if (j >= 1) {
+					sum += M[i - 1][j - 1];
+					count++;
+				}
+				if (j <= MColSize[i] - 2) {
+					sum += M[i - 1][j + 1];
+					count++;
+				}
+				sum += M[i - 1][j];
+				count++;
+			}
+			if (i <= MSize - 2) {
+				if (j >= 1) {
+					sum += M[i + 1][j - 1];
+					count++;
+				}
+				if (j <= MColSize[i] - 2) {
+					sum += M[i + 1][j + 1];
+					count++;
+				}
+				sum += M[i + 1][j];
+				count++;
+			}
+			if (j >= 1) {
+				sum += M[i][j - 1];
+				count++;
+			}
+			if (j <= MColSize[i] - 2) {
+				sum += M[i][j + 1];
+				count++;
+			}
+			sum += M[i][j];
+			count++;
+			ret[i][j] = sum / count;
+			*/
+			ret[i][j] = (((i >= 1) ? M[i - 1][j] : 0) + ((i <= MSize - 2) ? M[i + 1][j] : 0) + ((j >= 1) ? M[i][j - 1] : 0) + ((j <= MColSize[i] - 2) ? M[i][j + 1] : 0) + ((i >= 1 && j >= 1) ? M[i - 1][j - 1] : 0) + ((i >= 1 && j <= MColSize[i] - 2) ? M[i - 1][j + 1] : 0) + ((i <= MSize - 2 && j >= 1) ? M[i + 1][j - 1] : 0) + ((i <= MSize - 2 && j <= MColSize[i] - 2) ? M[i + 1][j + 1] : 0)  + M[i][j]) / (((i >= 1) ? 1 : 0) + ((i <= MSize - 2) ? 1 : 0) + ((j >= 1) ? 1 : 0) + ((j <= MColSize[i] - 2) ? 1 : 0) + ((i >= 1 && j >= 1) ? 1 : 0) + ((i >= 1 && j <= MColSize[i] - 2) ? 1 : 0) + ((i <= MSize - 2 && j >= 1) ? 1 : 0) + ((i <= MSize - 2 && j <= MColSize[i] - 2) ? 1 : 0) + 1);
+		}
+	}
+	return ret;
+}
