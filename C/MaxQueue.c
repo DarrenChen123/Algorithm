3ee2cf171d04bbad898279ef50df8a0206987b17
chenjia <chenjia2432@gmail.com>
2020-03-10 03:59:43 +0800

    队列的最大值。

--- C/MaxQueue.c
+++ C/MaxQueue.c
@@ -1,185 +1,4 @@
-/*
-请定义一个队列并实现函数 max_value 得到队列里的最大值，要求函数max_value、push_back 和 pop_front 的均摊时间复杂度都是O(1)。
-
-若队列为空，pop_front 和 max_value 需要返回 -1
-
-示例 1：
-
-输入: 
-["MaxQueue","push_back","push_back","max_value","pop_front","max_value"]
-[[],[1],[2],[],[],[]]
-输出: [null,null,null,2,1,2]
-示例 2：
-
-输入: 
-["MaxQueue","pop_front","max_value"]
-[[],[],[]]
-输出: [null,-1,-1]
- 
-
-限制：
-
-1 <= push_back,pop_front,max_value的总操作数 <= 10000
-1 <= value <= 10^5
-*/
-
-//typedef struct queue {
-//	int front;
-//	int rear;
-//	int items;
-//	int maxSize;
-//	int *arr;
-//} Queue;
-//
-//void initializeQueue (Queue * pq, int maxSize) {
-//	pq->front = 0;
-//	pq->rear = 0;
-//	pq->items = 0;
-//	pq->maxSize = maxSize;
-//	pq->arr = (int *)malloc(sizeof(int) * maxSize);
-//	memset(pq->arr, 0, sizeof(int) * maxSize);
-//}
-//
-//bool queueIsFull (const Queue * pq) {
-//	if ((pq->rear + 1) % (pq->maxSize) == pq->front) {
-//		return true;
-//	} else {
-//		return false;
-//	}
-//}
-//
-//bool queueIsEmpty (const Queue * pq) {
-//	return pq->items == 0;
-//}
-//
-//int queueItemCount (const Queue * pq) {
-//	return pq->items;
-//}
-//
-//bool enQueue (int item, Queue * pq) {
-//	if (queueIsFull(pq)) {
-//		return false;
-//	} else {
-//		pq->arr[pq->rear] = item;
-//		pq->rear = (pq->rear + 1) % 10;
-//		pq->items++;
-//		return true; 
-//	}
-//}
-//
-//bool deQueue(int* pitem, Queue * pq) {
-//	if (queueIsEmpty(pq)) {
-//		return false;
-//	}
-//	*pitem = pq->arr[pq->front];
-//	pq->front = (pq->front + 1) % (pq->maxSize);
-//	pq->items--;
-//	return true;
-//}
-//
-//typedef struct stack {
-//	int *items;
-//	int maxSize;
-//	int top;
-//} Stack;
-//
-//void initializeStack (Stack * ps, int maxSize) {
-//	int *items = (int *) malloc (sizeof(int) * maxSize);
-//	memset(items, 0, sizeof(int) * maxSize);
-//	ps->top = 0;
-//	ps->maxSize = maxSize;
-//	ps->items = items;
-//}
-//
-//bool fullStack (const Stack * ps) {
-//	return ps->top == ps->maxSize;
-//}
-//
-//bool emptyStack (const Stack * ps) {
-//	return ps->top == 0;
-//}
-//
-//bool push (int item, Stack * ps) {
-//	if (ps->top == ps->maxSize) {
-//		return false;
-//	} else {
-//		ps->items[ps->top++] = item;
-//		return true;
-//	}
-//}
-//
-//bool pop (int * pitem, Stack * ps) {
-//	if (ps->top == 0) {
-//		return false;
-//	} else {
-//		ps->top--;
-//		*pitem = ps->items[ps->top];
-//		return true;
-//	}
-//}
-//
-//int peek (const Stack * ps) {
-//	if (ps->top > 0) {
-//		return ps->items[ps->top - 1];
-//	} else {
-//		return INT_MIN;
-//	}
-//}
-//
-///** initialize your data structure here. */
-//typedef struct {
-//	Queue *queue;
-//	Stack *maxIndexStack;
-//} MaxQueue;
-//
-//
-//MaxQueue* maxQueueCreate() {
-//	MaxQueue *maxQueue = (MaxQueue *) malloc (sizeof(MaxQueue));
-//	Queue *queue = (Queue *) malloc (sizeof(Queue));
-//	initializeQueue(queue, 1024);
-//	Stack *maxIndexStack = (Stack *) malloc (sizeof(Stack));
-//	initializeStack(maxIndexStack, 1024);
-//	maxQueue->queue = queue;
-//	maxQueue->maxIndexStack = maxIndexStack;
-//	return maxQueue;
-//}
-//
-//int maxQueueMax_value(MaxQueue* obj) {
-//	if (peek(obj->maxIndexStack) >= 0) {
-//		return obj->queue->arr[peek(obj->maxIndexStack)];
-//	} else {
-//		return -1;
-//	}
-//}
-//
-//void maxQueuePush_back(MaxQueue* obj, int value) {
-//	if (!queueIsFull(obj->queue)) {
-//		enQueue(value, obj->queue);
-//	}
-//	if (emptyStack(obj->maxIndexStack) || value >= obj->queue->arr[peek(obj->maxIndexStack)]) {
-//		push (obj->queue->rear - 1, obj->maxIndexStack);
-//	}
-//}
-//
-//int maxQueuePop_front(MaxQueue* obj) {
-//	if (peek(obj->maxIndexStack) == obj->queue->front) {
-//		int i = 0;
-//		pop(&i, obj->maxIndexStack);
-//	}
-//	int x = -1;
-//	if (!emptyStack(obj->queue)) {
-//		deQueue(&x, obj->queue);
-//	}
-//	return x;
-//}
-//
-//void maxQueueFree(MaxQueue* obj) {    
-//	free(obj->queue->arr);
-//	free(obj->maxIndexStack->items);
-//	free(obj->queue);
-//	free(obj->maxIndexStack);
-//	free(obj);
-//}
+// 1.使用两个最大栈
 typedef struct stack {
 	int *items;
 	int maxSize;
@@ -351,5 +170,158 @@ int maxQueuePop_front(MaxQueue* obj) {
 void maxQueueFree(MaxQueue* obj) {    
 	maxStackFree(obj->s1);
 	maxStackFree(obj->s2);
+	free(obj);
+}
+
+
+// 2.使用双端队列
+typedef struct deque {
+	int front;
+	int rear;
+	int items;
+	int maxSize;
+	int *arr;
+} Deque;
+
+void initializeDeque (Deque * pq, int maxSize) {
+	pq->front = 0;
+	pq->rear = -1;
+	pq->items = 0;
+	pq->maxSize = maxSize;
+	pq->arr = (int *)malloc(sizeof(int) * maxSize);
+	memset(pq->arr, 0, sizeof(int) * maxSize);
+}
+
+bool dequeIsFull (const Deque * pq) {
+	if (pq->maxSize == pq->items) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool dequeIsEmpty (const Deque * pq) {
+	return pq->items == 0;
+}
+
+int dequeItemCount (const Deque * pq) {
+	return pq->items;
+}
+
+bool push_back (int item, Deque * pq) {
+	if (dequeIsFull(pq)) {
+		return false;
+	} else {
+		pq->rear = (pq->rear + 1) % (pq->maxSize);
+		pq->items++;
+		pq->arr[pq->rear] = item;
+		return true; 
+	}
+}
+
+bool push_front (int item, Deque * pq) {
+	if (dequeIsFull(pq)) {
+		return false;
+	} else {
+		pq->front = (pq->front - 1 + pq->maxSize) % (pq->maxSize);
+		pq->items++;
+		pq->arr[pq->front] = item;
+		return true;
+	}
+}
+
+bool pop_front(int* pitem, Deque * pq) {
+	if (dequeIsEmpty(pq)) {
+	   return false;
+	}
+	*pitem = pq->arr[pq->front];
+	pq->front = (pq->front + 1) % (pq->maxSize);
+	pq->items--;
+	return true; 
+}
+
+bool pop_back(int* pitem, Deque * pq) {
+	if (dequeIsEmpty(pq)) {
+	   return false;
+	}
+	*pitem = pq->arr[pq->rear];
+	pq->rear = (pq->rear - 1 + pq->maxSize) % (pq->maxSize);
+	pq->items--;
+	return true; 
+}
+
+int peek_front (const Deque * pq) {
+	if (!dequeIsEmpty(pq)) {
+		return pq->arr[pq->front];
+	} else {
+		return INT_MIN;
+	}
+}
+
+int peek_back (const Deque * pq) {
+	if (!dequeIsEmpty(pq)) {
+		return pq->arr[pq->rear];
+	} else {
+		return INT_MIN;
+	}
+}
+
+/** initialize your data structure here. */
+
+typedef struct {
+	Deque *deque;
+	Deque *maxIndexDeque;
+} MaxQueue;
+
+
+MaxQueue* maxQueueCreate() {
+	MaxQueue *maxQueue = (MaxQueue *) malloc (sizeof(MaxQueue));
+	Deque *deque = (Deque *) malloc (sizeof(Deque));
+	initializeDeque(deque, 1024);
+	Deque *maxIndexDeque = (Deque *) malloc (sizeof(Deque));
+	initializeDeque(maxIndexDeque, 1024);
+	maxQueue->deque = deque;
+	maxQueue->maxIndexDeque = maxIndexDeque;
+	return maxQueue;
+}
+
+int maxQueueMax_value(MaxQueue* obj) {
+	if (peek_front(obj->maxIndexDeque) >= 0) {
+		return obj->deque->arr[peek_front(obj->maxIndexDeque)];
+	} else {
+		return -1;
+	}
+}
+
+void maxQueuePush_back(MaxQueue* obj, int value) {
+	while (!dequeIsEmpty(obj->maxIndexDeque) && obj->deque->arr[peek_back(obj->maxIndexDeque)] < value) {
+		int i = 0;
+		pop_back(&i, obj->maxIndexDeque);
+	}
+	if (!dequeIsFull(obj->deque)) {
+		push_back(value, obj->deque);
+	}
+	if (!dequeIsFull(obj->maxIndexDeque)) {
+		push_back((obj->deque->rear) % obj->deque->maxSize, obj->maxIndexDeque);
+	}
+}
+
+int maxQueuePop_front(MaxQueue* obj) {
+	if (peek_front(obj->maxIndexDeque) == obj->deque->front) {
+		int i = 0;
+		pop_front(&i, obj->maxIndexDeque);
+	}
+	int x = -1;
+	if (!dequeIsEmpty(obj->deque)) {
+		pop_front(&x, obj->deque);
+	}
+	return x;
+}
+
+void maxQueueFree(MaxQueue* obj) {    
+	free(obj->deque->arr);
+	free(obj->maxIndexDeque->arr);
+	free(obj->deque);
+	free(obj->maxIndexDeque);
 	free(obj);
 }
