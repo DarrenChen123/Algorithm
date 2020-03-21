98839d3efb1b7a3e8d96258afabb2dcef4fd3935
chenjia <chenjia2432@gmail.com>
2020-03-22 00:53:34 +0800

    Find the distance between two arrays.

---
+++ C/FindTheDistanceValueBetweenTwoArrays.c
@@ -0,0 +1,59 @@
+/*
+Given two integer arrays arr1 and arr2, and the integer d, return the distance value between the two arrays.
+
+The distance value is defined as the number of elements arr1[i] such that there is not any element arr2[j] where |arr1[i]-arr2[j]| <= d.
+
+ 
+
+Example 1:
+
+Input: arr1 = [4,5,8], arr2 = [10,9,1,8], d = 2
+Output: 2
+Explanation: 
+For arr1[0]=4 we have: 
+|4-10|=6 > d=2 
+|4-9|=5 > d=2 
+|4-1|=3 > d=2 
+|4-8|=4 > d=2 
+For arr1[1]=5 we have: 
+|5-10|=5 > d=2 
+|5-9|=4 > d=2 
+|5-1|=4 > d=2 
+|5-8|=3 > d=2
+For arr1[2]=8 we have:
+|8-10|=2 <= d=2
+|8-9|=1 <= d=2
+|8-1|=7 > d=2
+|8-8|=0 <= d=2
+Example 2:
+
+Input: arr1 = [1,4,2,3], arr2 = [-4,-3,6,10,20,30], d = 3
+Output: 2
+Example 3:
+
+Input: arr1 = [2,1,100,3], arr2 = [-5,-2,10,-3,7], d = 6
+Output: 1
+ 
+
+Constraints:
+
+1 <= arr1.length, arr2.length <= 500
+-10^3 <= arr1[i], arr2[j] <= 10^3
+0 <= d <= 100
+*/
+
+int findTheDistanceValue(int* arr1, int arr1Size, int* arr2, int arr2Size, int d){
+	int ret = 0;
+	for (int i = 0; i < arr1Size; i++) {
+		bool flag = true;
+		for (int j = 0; j < arr2Size; j++) {
+			if ((arr1[i] >= arr2[j] && arr1[i] - arr2[j] <= d) || (arr2[j] >= arr1[i] && arr2[j] - arr1[i] <= d)) {
+				flag = false;
+			}
+		}
+		if (flag) {
+			ret++;
+		}
+	}
+	return ret;
+}
